59c399e216bb862654af33c9f7fb242ff49c8d66
marianorivero <marianorivero97@gmail.com>
2022-04-08 19:15:07 -0300

    clase 8-4

---
+++ trabajo2/js/scripts/funciones.js
@@ -0,0 +1,25 @@
+function saludar() {
+    console.log("Hola Mundo")
+}
+
+function hola() {
+    console.log("Hola");
+}
+
+function chau() {
+    console.log("Chau");
+}
+
+function funcionDiv() {
+    alert("Pasaste por aca")
+}
+
+function cambiar() {
+    document.getElementById("a1").style.color= "red";
+    document.getElementById("a1").style.backgroundColor = "black";
+}
+
+function original() {
+    document.getElementById("a1").style.color= "black";
+    document.getElementById("a1").style.backgroundColor = "blue";
+}
