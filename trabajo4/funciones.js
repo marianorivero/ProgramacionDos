88a4db1b0b6f535f6f65e68e76953f583aaffb55
mariano rivero <marianorivero97@gmail.com>
2022-04-28 21:31:15 -0300

    clase 28-04-22

--- trabajo4/funciones.js
+++ trabajo4/funciones.js
@@ -1,12 +1,12 @@
 function boton() {
     let arreglo = [99, 4, 34, 93, 1];
     
-    arreglo.sort(function(a, b){return a - b});
+    arreglo.sort(function(a, b){return a - b});//ordenar
     arreglo.forEach(elemento => console.log(elemento))
 
     console.log("");
 
-    arreglo.forEach(function(elemento){
+    arreglo.forEach(function(elemento){//mostrar mayores a 50
         if (elemento > 50) {
             console.log(elemento);
         }
@@ -15,7 +15,7 @@ function boton() {
     
     console.log("");
 
-    arreglo.forEach(function(elemento){
+    arreglo.forEach(function(elemento){ //mostrar solo los pares
         if ((elemento %2) == 0) {
             console.log(elemento);
         }
