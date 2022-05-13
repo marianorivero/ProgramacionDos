277770719649aabcca99188cb03c9cc7601132fd
mariano rivero <marianorivero97@gmail.com>
2022-05-12 21:11:51 -0300

    clase 12-05-21

---
+++ trabajo6/funciones.js
@@ -0,0 +1,54 @@
+let superHeroes = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+      {
+        "name": "Capitan America",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": [
+          "Radiation resistance",
+          "Turning tiny",
+          "Radiation blast"
+        ]
+      },
+      {
+        "name": "Iron Man",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+          "Million tonne punch",
+          "Damage resistance",
+          "Superhuman reflexes"
+        ]
+      },
+      {
+        "name": "Sub-cero",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+          "Immortality",
+          "Heat Immunity",
+          "Inferno",
+          "Teleportation",
+          "Interdimensional travel"
+        ]
+      }
+    ]
+  }
+
+
+function traer() {
+    let div = document.getElementById("div")
+
+    superHeroes.members.forEach(element => {
+        if (element.age >100) {
+            div.textContent = `Nombre: ${element.name}`
+        }
+    });
+}
+
+
