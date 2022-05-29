c11d6037dda563d1c2e67081072c5929bfa18028
Mariano Rivero <marianorivero97@gmail.com>
2022-05-29 13:45:50 -0300

    refactorizacion trabajo10

--- trabajo10/formulario.php
+++ trabajo10/formulario.php
@@ -6,48 +6,24 @@ $fechaNacimiento=$_POST['date'];
 $sexo=$_POST["sexo"];
 //validacion de que DNI sea un numero
 if (is_numeric($_POST["dni"])) {
-    $dni= $_POST["dni"]; 
-}
+    $dni= $_POST["dni"]; }
+
+
 //Declaro el arreglo colores
 $colores = array ();
-
-
-
-//Valido si el checkbox no es nulo, si no lo es pusheo
-/*if(isset($_POST["verde"])){
-    array_push($colores,"verde");
-};
-if(isset($_POST["amarillo"])){
-    array_push($colores,"amarillo");
-};
-if(isset($_POST["azul"])){
-    array_push($colores,"azul");
-};
-if(isset($_POST["rojo"])){
-    array_push($colores,"rojo");
-};*/
-
-
-
-$indices = array ("verde","amarillo","azul","rojo");
-foreach($indices as &$indice){
-    if (isset($_POST[$indice])) {
-        array_push($colores,$indice);
-    }
-}
-
-
-
+//capturo checkbox
+if (isset($_POST["colores"])) { //tambien se puede usar !empty
+    foreach ($_POST["colores"] as $color) {array_push($colores,$color);}
+} 
 
 //Muestro resultados en pantalla
-echo "Nombre: ".$nombre."<br><br>";
-echo "Apellido: ".$apellido."<br><br>";
-echo "DNI: ".$dni."<br><br>";
-echo "Fecha de nacimiento: ".$fechaNacimiento."<br><br>";
-echo "Sexo: ".$sexo."<br><br>";
-echo "Colores:<br>";
+echo "<b>Nombre:</b> ".$nombre."<br><br>";
+echo "<b>Apellido:</b> ".$apellido."<br><br>";
+echo "<b>DNI:</b> ".$dni."<br><br>";
+echo "<b>Fecha de nacimiento:</b> ".$fechaNacimiento."<br><br>";
+echo "<b>Sexo:</b> ".$sexo."<br><br>";
+echo "<b>Colores:</b> <br>";
 foreach($colores as &$color){echo " -".$color."<br>";}
-
 ?>
 
 
