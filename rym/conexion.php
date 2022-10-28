36b0a56fd5571777610b6f57592f198379c3b1e8
Mariano Rivero <marianorivero97@gmail.com>
2022-10-27 23:22:46 -0300

    27-10-22

--- rym/conexion.php
+++ rym/conexion.php
@@ -6,14 +6,20 @@ $especie = $_POST["especie"];
 $estado = $_POST["estado"];
 
 
+
 $conexion = mysqli_connect("localhost","root","","rym");
 
-$consulta = "INSERT INTO `personajes`(`id`,`nombre`, `especie`, `estado`) VALUES ('{$id}','{$nombre}','{$especie}','{$estado}'')";
+$consultaSelect = "SELECT * FROM `personajes` WHERE `id`='{$id}' AND `nombre`='{$nombre}' AND `especie`='{$especie}' AND `estado`='{$estado}'";
+$consultaInsert = "INSERT INTO personajes(id,nombre,especie,estado) VALUES('{$id}','{$nombre}','{$especie}','{$estado}')";
 
-$res = mysqli_query($conexion,$consulta);
 
-// `id`,
-// '{$id}',
-echo "Conexion exitosa";
+$select = mysqli_query($conexion,$consultaSelect);
+$datos = mysqli_fetch_all($select,MYSQLI_ASSOC);
 
+if (!empty($datos)) {
+    echo "El personaje ya se encuentra registrado";
+}else{
+    $insert = mysqli_query($conexion,$consultaInsert);
+    echo "Personaje registrado con exito";
+}
 ?>
